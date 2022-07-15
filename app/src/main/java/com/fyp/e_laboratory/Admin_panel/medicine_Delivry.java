9b3a38c5dd6d6be95b87f18e8f8541947d284f0c
Asim Mehar <asimmeharproffessional@gmail.com>
2022-07-16 00:20:23 +0500

    E Lab

---
+++ app/src/main/java/com/fyp/e_laboratory/Admin_panel/medicine_Delivry.java
@@ -0,0 +1,16 @@
+package com.fyp.e_laboratory.Admin_panel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fyp.e_laboratory.R;
+
+public class medicine_Delivry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicine_delivry);
+    }
+}
