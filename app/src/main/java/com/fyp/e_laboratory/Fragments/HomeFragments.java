24f50481648aa4853a477d10419675d98372ee6d
Asim Mehar <asimmeharproffessional@gmail.com>
2022-07-15 16:00:10 +0500

    E Lab

--- app/src/main/java/com/fyp/e_laboratory/Fragments/HomeFragments.java
+++ app/src/main/java/com/fyp/e_laboratory/Fragments/HomeFragments.java
@@ -2,6 +2,7 @@ package com.fyp.e_laboratory.Fragments;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -15,10 +16,20 @@ public class HomeFragments extends Fragment {
 
 
 
+    CardView medi,aid,ambulance,apoinment,report;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_fragments, container, false);
+        View v= inflater.inflate(R.layout.fragment_home_fragments, container, false);
+
+        medi=v.findViewById(R.id.medicine);
+        aid=v.findViewById(R.id.aid);
+        ambulance=v.findViewById(R.id.ambulance);
+        apoinment=v.findViewById(R.id.apoinment);
+        report=v.findViewById(R.id.report);
+
+
+        return v;
     }
 }
