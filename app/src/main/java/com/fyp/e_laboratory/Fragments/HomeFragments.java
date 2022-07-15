1734032026878cc4fdadc45cc9e5f6b9f063aa36
Asim Mehar <asimmeharproffessional@gmail.com>
2022-07-15 20:25:09 +0500

    E Lab

--- app/src/main/java/com/fyp/e_laboratory/Fragments/HomeFragments.java
+++ app/src/main/java/com/fyp/e_laboratory/Fragments/HomeFragments.java
@@ -1,8 +1,13 @@
 package com.fyp.e_laboratory.Fragments;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.fyp.e_laboratory.R;
+import com.fyp.e_laboratory.UserPanel.Instruction;
 
 
 public class HomeFragments extends Fragment {
@@ -29,6 +35,26 @@ public class HomeFragments extends Fragment {
         apoinment=v.findViewById(R.id.apoinment);
         report=v.findViewById(R.id.report);
 
+        ambulance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:1122"));
+
+                if (ActivityCompat.checkSelfPermission(getContext(),
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
+        aid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(), Instruction.class));
+
+            }
+        });
 
         return v;
     }
---
+++ app/src/main/java/com/fyp/e_laboratory/UserPanel/Instruction.java
@@ -0,0 +1,16 @@
+package com.fyp.e_laboratory.UserPanel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fyp.e_laboratory.R;
+
+public class Instruction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instruction);
+    }
+}
