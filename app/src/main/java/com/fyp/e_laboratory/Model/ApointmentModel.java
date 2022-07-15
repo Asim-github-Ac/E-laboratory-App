57e8dae4a4eb799d326c0afabce4178db8badd4e
Asim Mehar <asimmeharproffessional@gmail.com>
2022-07-15 23:18:53 +0500

    E Lab

---
+++ app/src/main/java/com/fyp/e_laboratory/Model/ApointmentModel.java
@@ -0,0 +1,66 @@
+package com.fyp.e_laboratory.Model;
+
+public class ApointmentModel {
+
+    String name,email,phone,address,time,uid;
+
+    public ApointmentModel(String name, String email, String phone, String address, String time, String uid) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.time = time;
+        this.uid = uid;
+    }
+
+    public ApointmentModel() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+}
--- app/src/main/java/com/fyp/e_laboratory/UserPanel/ApointMentBook.java
+++ app/src/main/java/com/fyp/e_laboratory/UserPanel/ApointMentBook.java
@@ -1,24 +1,42 @@
 package com.fyp.e_laboratory.UserPanel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import com.fyp.e_laboratory.Model.ApointmentModel;
 import com.fyp.e_laboratory.R;
 import com.fyp.e_laboratory.SharedPrefrence.PrefManager;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ApointMentBook extends AppCompatActivity {
     TimePicker timepicker;
-    Button changetime;
+    Button changetime,submit;
     String uid;
     String time;
+    EditText emails,names,phones,addresss;
+    ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apoint_ment_book);
+        names=findViewById(R.id.ap_name);
+        emails=findViewById(R.id.ap_email);
+        addresss=findViewById(R.id.ap_address);
+        phones=findViewById(R.id.ap_phone);
+        progressBar=findViewById(R.id.ap_pbbar);
+
         PrefManager prefManager=new PrefManager(getApplicationContext());
         uid=prefManager.getUserID();
 
@@ -26,18 +44,46 @@ public class ApointMentBook extends AppCompatActivity {
         //Uncomment the below line of code for 24 hour view
         timepicker.setIs24HourView(true);
         changetime=(Button)findViewById(R.id.button1);
-
+        System.out.println("time is "+time);
         time=getCurrentTime();
         changetime.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
               time= getCurrentTime();
+                System.out.println("time is "+time);
             }
         });
 
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                AddApointment(names.getText().toString(),emails.getText().toString(),phones.getText().toString(),addresss.getText().toString(),time,uid);
+            }
+        });
     }
     public String getCurrentTime(){
         String currentTime="Current Time: "+timepicker.getCurrentHour()+":"+timepicker.getCurrentMinute();
         return currentTime;
+    }
+    public void AddApointment(String name,String email,String phone,String addres,String tim,String uid){
+
+        DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference("ELab_Apointment");
+        ApointmentModel apointmentModel=new ApointmentModel(name,email,phone,addres,tim,uid);
+
+        databaseReference.child(uid).setValue(apointmentModel).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                progressBar.setVisibility(View.INVISIBLE);
+                Toast.makeText(ApointMentBook.this, "ApointMent Added", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                progressBar.setVisibility(View.INVISIBLE);
+                Toast.makeText(ApointMentBook.this, "Something Went Wrong", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
