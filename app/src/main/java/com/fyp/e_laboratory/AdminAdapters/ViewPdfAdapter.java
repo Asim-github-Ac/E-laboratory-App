176c38f65607eb7966182b5d37ba8ab08acd6b62
Asim Mehar <asimmeharproffessional@gmail.com>
2022-07-18 12:59:18 +0500

    E Lab

--- app/src/main/java/com/fyp/e_laboratory/AdminAdapters/ViewPdfAdapter.java
+++ app/src/main/java/com/fyp/e_laboratory/AdminAdapters/ViewPdfAdapter.java
@@ -1,6 +1,12 @@
 package com.fyp.e_laboratory.AdminAdapters;
 
+import static android.content.Context.DOWNLOAD_SERVICE;
+
+import android.app.DownloadManager;
 import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,12 +19,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.fyp.e_laboratory.Model.PdfModel;
 import com.fyp.e_laboratory.R;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 public class ViewPdfAdapter extends RecyclerView.Adapter<ViewPdfAdapter.myHolder> {
     Context context;
     List<PdfModel> pdfModelList;
-
+    URL url;
     public ViewPdfAdapter(Context context, List<PdfModel> pdfModelList) {
         this.context = context;
         this.pdfModelList = pdfModelList;
@@ -41,6 +50,7 @@ public class ViewPdfAdapter extends RecyclerView.Adapter<ViewPdfAdapter.myHolder
         holder.dow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                downloadPdf(pdfModel.getPdfurl());
 
             }
         });
@@ -61,5 +71,36 @@ public class ViewPdfAdapter extends RecyclerView.Adapter<ViewPdfAdapter.myHolder
             dow=itemView.findViewById(R.id.downpdf);
 
         }
+    }
+    private void downloadPdf(String pdfURL) {
+
+        try {
+            url = new URL(pdfURL);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            //  requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},READ_STORAGE_PERMISSION_REQUEST_CODE);
+        }
+
+        File direct = new File(Environment.getExternalStorageDirectory() + "/Download/AldoFiles");
+
+        if (!direct.exists()) {
+            File pdfDirectory = new File("/sdcard/Download/AldoFiles/");
+            pdfDirectory.mkdirs();
+        }
+
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url + ""));
+        request.setTitle("pdf");
+        request.setMimeType("application/pdf");
+        request.allowScanningByMediaScanner();
+        request.setAllowedOverMetered(true);
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,"pdf");
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService(DOWNLOAD_SERVICE);
+        downloadManager.enqueue(request);
+
     }
 }
